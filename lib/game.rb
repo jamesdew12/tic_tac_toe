58dd61168cabcaee4a94445e4e8acbf437c42f79
James de Winton <james.dewinton@gmail.com>
2019-02-26 11:11:22 +0000

    board initially empty

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,15 @@
 class Game
-  attr_reader :a1
+  attr_reader :a1, :a2, :a3, :b1, :b2, :b3, :c1, :c2, :c3
   def initialize
     @a1 = ""
+    @a2 = ""
+    @a3 = ""
+    @b1 = ""
+    @b2 = ""
+    @b3 = ""
+    @c1 = ""
+    @c2 = ""
+    @c3 = ""
 
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,14 @@ require 'game'
 describe Game do
   it "board starts empty" do
     expect(subject.a1()).to eq ""
-
+    expect(subject.a2()).to eq ""
+    expect(subject.a3()).to eq ""
+    expect(subject.b1()).to eq ""
+    expect(subject.b2()).to eq ""
+    expect(subject.b3()).to eq ""
+    expect(subject.c1()).to eq ""
+    expect(subject.c2()).to eq ""
+    expect(subject.c3()).to eq ""
   end
 
 end
